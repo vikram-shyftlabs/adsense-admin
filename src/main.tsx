606d5d737db38d91bb97983d6a3eec883b0a8045
Purav308 <purav.shah@shyftlabs.io>
2024-04-17 03:06:11 +0530

    Added Persistor

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,9 @@ import SidebarComponent from "./components/sidebar/sidebar.component.jsx";
 import Login from "./pages/accout/account.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
-import store from "./redux/store/store.js";
+import store,{persistor} from "./redux/store/store.js";
 import Account from "./pages/account/account.component.tsx";
+import { PersistGate } from 'redux-persist/integration/react';
 
 const router = createBrowserRouter([
   {
@@ -29,9 +30,11 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId="905703355143-hbpbqk2bmpnjocs9j3h4r4537qmru1ma.apps.googleusercontent.com">
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <React.StrictMode>
         <RouterProvider router={router} />
       </React.StrictMode>
+    </PersistGate>
     </Provider>
   </GoogleOAuthProvider>
 );
