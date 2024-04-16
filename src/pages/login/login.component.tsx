51c212d4ac358557acc635a8e3f3f915d732958d
Purav308 <purav.shah@shyftlabs.io>
2024-04-17 01:52:32 +0530

    Added google login

--- src/pages/login/login.component.tsx
+++ src/pages/login/login.component.tsx
@@ -1,79 +1,75 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import FacebookLogin from "react-facebook-login";
-import { GoogleLogin } from "@react-oauth/google";
+import React, {useState} from 'react';
+import { Button, Card } from 'antd';
+import { FacebookOutlined ,GoogleOutlined} from '@ant-design/icons';
+import { LoginSocialGoogle, LoginSocialFacebook } from 'reactjs-social-login';
+
+const clientId =  '201891706446-0rtnh7ickkgeve8aol9d3f6k1uthso6e.apps.googleusercontent.com'; // Replace with your Google OAuth2 Client ID
+
 
 const Login = () => {
-  const thirdPartyLoginHandler = ({ response, provider, error }: any) => {
-    // console.log(" response>>", response)
-    // console.log(" provider>>", provider)
-    // console.log(" error>>", error)
-    console.log(response, provider, error, "response, provider, error");
-    // dispatch(login({ user: response, provider, error }));
-  };
+    const [provider, setProvider] = useState(null);
+    const [data, setData] = useState({});
 
-  const responseFacebook = (response: {
-    status: string | undefined;
-    error: any;
-  }) => {
-    // console.log('response >>>', response);
-    if (
-      response.status === "unknown" ||
-      response.status === undefined ||
-      response.error
-    )
-      return thirdPartyLoginHandler({
-        error: true,
-        provider: "facebook",
-        response: {},
-      });
+    const onLogin=({ provider, data }) => {
+        console.log(data);
+        setProvider(provider);
+        setData(data)
+    }
 
-    thirdPartyLoginHandler({ error: false, provider: "facebook", response });
-  };
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card style={{ width: 300 }}>
+                <h2 style={{ textAlign: 'center' }}>Login</h2>
+             <div style={{ display: 'flex',flexDirection:'column', justifyContent: 'space-evenly', alignItems: 'center' ,minHeight:'10rem'}}>
+                 <LoginSocialGoogle
+                     isOnlyGetToken
+                     client_id={clientId}
+                     onLoginStart={()=>console.log('Login start')}
+                     redirect_uri={'http://localhost:5173'}
+                     onResolve={onLogin}
+                     onReject={err => {
+                         console.log(err);
+                     }}
+                 >
+                     <Button
+                         type="primary"
+                         icon={<GoogleOutlined />}
+                         style={{ width: '100%' }}
+                     >
+                         Login with Google
+                     </Button>
+                 </LoginSocialGoogle>
 
-  return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account with Social Logins
-        </h2>
-      </div>
+                 <LoginSocialFacebook
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <FacebookLogin
-            appId="2192834227588742"
-            fields="name,email,picture"
-            callback={responseFacebook}
-            textButton="Login with Facebook"
-          />
-          {/* clientId="905703355143-hbpbqk2bmpnjocs9j3h4r4537qmru1ma.apps.googleusercontent.com" */}
-          <GoogleLogin
-            onSuccess={(credentialResponse: any) => {
-              console.log(credentialResponse);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        </form>
+                     appId='2192834227588742'
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member? &nbsp;
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+                     onLoginStart={()=>console.log('Login start')}
+                     onResolve={onLogin}
+                     onReject={err => {
+                         console.log(err);
+                     }}
+                 >
+                     <Button
+                         type="primary"
+                         icon={<FacebookOutlined />}
+                         style={{ width: '100%' }}
+                     >
+                         Login with Facebook
+                     </Button>
+                 </LoginSocialFacebook>
+             </div>
+                {/*<LoginSocialFacebook*/}
+                {/*    appId="YOUR_FACEBOOK_APP_ID"*/}
+                {/*    onSuccess={handleFacebookLogin}*/}
+                {/*    onFailure={(error) => console.error('Facebook login failed:', error)}*/}
+                {/*    style={{ width: '100%' }}*/}
+                {/* onReject={(v)=>console.log(v)}*/}
+                {/*    onResolve={}/>*/}
+            </Card>
+        </div>
+    );
 };
+
 
 export default Login;
