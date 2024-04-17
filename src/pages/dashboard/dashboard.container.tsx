dbe81fe809a512148ddf4001a44cccea77e638c8
lovedeepshyftlabs <lovedeep@shyftlabs.io>
2024-04-17 14:52:31 +0530

    dashboard-charts-added

--- src/pages/dashboard/dashboard.container.tsx
+++ src/pages/dashboard/dashboard.container.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../../components/header/header.component";
 import PieChart from "../../components/dashboard/PieChart.jsx";
+import LineChartSetUp from "../../components/dashboard/LineChart/linechart-setup";
+
 const Dashboard: React.FC = () => {
   return (
     <div>
       <Header props={{ title: "Dashboard" }} />
+      <LineChartSetUp />
       <div className="grid grid-cols-2 m-12 rounded-md">
         <div className="p-8 border-[#dcdcdc] border rounded-md">
           <h1 className="font-semibold text-xl">By Platform</h1>
